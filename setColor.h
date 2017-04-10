1b0ca89d956fd02c13b1216568e32fda24596f3c
caiyj <caiyj@umich.edu>
2017-04-10 19:03:51 -0400

    Delete setColor.h

--- setColor.h
+++
@@ -1,9 +0,0 @@
-#include "ColorClass.h"
-
-using namespace std;
-
-//Programmer : Yingjie Cai
-//Date: Mar 2017
-//Purpose: Provide a header file to describe a global function - setColor.
-
-ColorClass setColor(int choiceOfItem);
